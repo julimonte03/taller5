2e0ace70db02397d4d6268c426d9a6ff72175652
Julian Monte <julianmonte2003@gmail.com>
2024-05-24 18:45:49 -0300

    insertar hice

--- src/main/java/aed/ABB.java
+++ src/main/java/aed/ABB.java
@@ -6,32 +6,89 @@ import java.util.*;
 // elem1.compareTo(elem2) devuelve un entero. Si es mayor a 0, entonces elem1 > elem2
 public class ABB<T extends Comparable<T>> implements Conjunto<T> {
     // Agregar atributos privados del Conjunto
+    private Nodo raiz; 
+    private int cardinal;
 
     private class Nodo {
-        // Agregar atributos privados del Nodo
 
-        // Crear Constructor del nodo
+        T valor;
+        Nodo izq;
+        Nodo der;
+        Nodo padre;
+        Nodo (T v){
+            valor = v;
+            izq = null;
+            der = null;
+            padre = null;
+        }
     }
 
     public ABB() {
-        throw new UnsupportedOperationException("No implementada aun");
+        raiz = null;
+        cardinal = 0;
     }
 
     public int cardinal() {
-        throw new UnsupportedOperationException("No implementada aun");
+        return cardinal;
     }
 
     public T minimo(){
-        throw new UnsupportedOperationException("No implementada aun");
+
+        Nodo minimo = raiz;
+        Nodo hijoMenor = raiz.izq;
+
+        while(hijoMenor != null){
+            minimo = hijoMenor;
+            hijoMenor = hijoMenor.izq;
+        }
+        
+        return minimo.valor;
     }
 
     public T maximo(){
-        throw new UnsupportedOperationException("No implementada aun");
-    }
+        
+        Nodo maximo = raiz;
+        Nodo hijoMayor = raiz.der;
 
-    public void insertar(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
+        while (hijoMayor != null) {
+            maximo = hijoMayor;
+            hijoMayor = hijoMayor.der;
+        }
+
+        return maximo.valor;
     }
+    public void insertar(T elem) {
+        if (raiz == null) {
+            raiz = new Nodo(elem);
+            cardinal++;
+            return;
+        }
+    
+        Nodo actual = raiz;
+        Nodo padre = null;
+    
+        while (actual != null) {
+            padre = actual;
+            if (elem.compareTo(actual.valor) < 0) {
+                actual = actual.izq;
+            } else if (elem.compareTo(actual.valor) > 0) {
+                actual = actual.der;
+            } else {
+                return;
+            }
+        }
+    
+        Nodo nuevo = new Nodo(elem);
+        nuevo.padre = padre; 
+    
+        if (elem.compareTo(padre.valor) < 0) {
+            padre.izq = nuevo;
+        } else {
+            padre.der = nuevo;
+        }
+    
+        cardinal++;
+    }    
 
     public boolean pertenece(T elem){
         throw new UnsupportedOperationException("No implementada aun");
